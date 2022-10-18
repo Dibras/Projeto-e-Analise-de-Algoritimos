4707ebd7819a2f07d6c9fde57555dd4169f941af
Vilela <47867188+gcvilela@users.noreply.github.com>
2022-10-18 20:12:30 -0300

    Update lcs_matriz.java

--- prog_dinamica/lcs_matriz.java
+++ prog_dinamica/lcs_matriz.java
@@ -6,11 +6,11 @@ public class Main
 	    String [] x = "gustavo".split();
 	    String [] y = "guvvvvv".split();
 		int[][] l = new int [x.length() + 1][x.length() + 1];
-		
+		lcs(l,x,y);
 	}
 	public static void lcs (int[][] l,String[] x, String[] y ){
-	    for(int i = 1; i <= y.length(); i++ ){
-	        for(int j = 1 ; j <= x.length(); j++){
+	    for(int i = 1; i <= y.length; i++ ){
+	        for(int j = 1 ; j <= x.length; j++){
 	            if (y[i - 1]==x[j - 1]){
 	                l[i][j] = l[i - 1][j - 1]+ 1;
 	            }else{
