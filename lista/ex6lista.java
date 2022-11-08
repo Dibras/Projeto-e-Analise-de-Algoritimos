cacba895a03841bfdd0e5bd8014606afaeac8bb2
Vilela <47867188+gcvilela@users.noreply.github.com>
2022-11-08 19:37:36 -0300

    Create ex6lista.java

---
+++ lista/ex6lista.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author 0038160
+ */
+public class JavaApplication3 {
+    public static void main(String[] args) {
+          int[] esteira1 ={10,15,9,15};
+          int[] esteira2 ={5,7,5,10};
+          int[] esteira3 ={12,5,2,14};
+          int posi = 0;
+          
+          for(int i = 0; i < esteira1.length; i++ ){
+              int maior = 0;
+
+              if (posi = 1 || posi = 2){
+                        if (esteira1[i] > maior){
+                            maior = esteira1[i] ;
+                            posi = 1;
+                        }
+                        if (esteira2[i] > maior){
+                            maior = esteira2[i] ;
+                            posi = 2;
+                        }
+          }
+              if (esteira3[i] > maior){
+                  maior = esteira3[i] ;
+                  posi = 3;
+              }
+              
+              
+              
+          }
+    }
+}
